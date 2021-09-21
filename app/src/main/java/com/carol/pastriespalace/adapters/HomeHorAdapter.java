588dc765bdc103cb5cc6088365bf6cccacf470c9
trevoromondi <otienomondi23@gmail.com>
2021-09-21 20:42:16 +0300

    Main Page

---
+++ app/src/main/java/com/carol/pastriespalace/adapters/HomeHorAdapter.java
@@ -0,0 +1,161 @@
+package com.carol.pastriespalace.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.carol.pastriespalace.R;
+import com.carol.pastriespalace.models.HomeHorModel;
+import com.carol.pastriespalace.models.HomeVerModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHolder> {
+
+
+    UpdateVerticalRec updateVerticalRec;
+    Activity activity;
+    ArrayList<HomeHorModel> list;
+
+    boolean check = true;
+    boolean select= true;
+    int row_index = -1;
+
+    public HomeHorAdapter(UpdateVerticalRec updateVerticalRec, Activity activity, ArrayList<HomeHorModel> list) {
+        this.updateVerticalRec = updateVerticalRec;
+        this.activity = activity;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_horizonatal_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.imageView.setImageResource(list.get(position).getImage());
+        holder.name.setText(list.get(position).getName());
+
+        if (check) {
+            ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+            homeVerModels.add(new HomeVerModel(R.drawable.pie2, "Pie", "10-23", "4.6", "Ksh.3000"));
+            homeVerModels.add(new HomeVerModel(R.drawable.pie3, "Pie", "10-23", "4.6", "Ksh.3000"));
+            homeVerModels.add(new HomeVerModel(R.drawable.pie4, "Pie", "10-23", "4.6", "Ksh.3000"));
+            homeVerModels.add(new HomeVerModel(R.drawable.pie4, "Pie", "10-23", "4.6", "Ksh.3000"));
+
+            updateVerticalRec.callBack(position, homeVerModels);
+            check = false;
+        }
+
+            holder.cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   row_index = position;
+                   notifyDataSetChanged();
+
+                   if(position == 0){
+                       ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                       homeVerModels.add(new HomeVerModel(R.drawable.pie2, "Pie", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.pie3, "Pie", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.pie4, "Pie", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.pie4, "Pie", "10-23","4.6","Ksh.3000"));
+
+                       updateVerticalRec.callBack(position, homeVerModels);
+                   }
+                   else if(position == 1){
+                       ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake2, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake3, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake4, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake6, "Cake", "10-23","4.6","Ksh.3000"));
+
+                       updateVerticalRec.callBack(position, homeVerModels);
+
+                   }
+
+                   else if(position == 2){
+                       ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake2, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake3, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake4, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake6, "Cake", "10-23","4.6","Ksh.3000"));
+
+                       updateVerticalRec.callBack(position, homeVerModels);
+
+                   }
+                   else if(position == 3){
+                       ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake2, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake3, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake4, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake6, "Cake", "10-23","4.6","Ksh.3000"));
+
+                       updateVerticalRec.callBack(position, homeVerModels);
+
+                   }
+                   else if(position == 4){
+                       ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake2, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake3, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake4, "Cake", "10-23","4.6","Ksh.3000"));
+                       homeVerModels.add(new HomeVerModel(R.drawable.cake6, "Cake", "10-23","4.6","Ksh.3000"));
+
+                       updateVerticalRec.callBack(position, homeVerModels);
+
+                   }
+
+                }
+            });
+
+            if(select){
+                if(position == 0){
+                    holder.cardView.setBackgroundResource(R.drawable.change_bg);
+                    select = false;
+                }
+            }
+            else {
+                if(row_index == position){
+                    holder.cardView.setBackgroundResource(R.drawable.change_bg);
+                }else{
+                    holder.cardView.setBackgroundResource(R.drawable.default_bg);
+                }
+            }
+        }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name;
+        CardView cardView;
+
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.hor_img);
+            name = itemView.findViewById(R.id.hor_text);
+            cardView = itemView.findViewById(R.id.cardView);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/carol/pastriespalace/adapters/UpdateVerticalRec.java
@@ -0,0 +1,9 @@
+package com.carol.pastriespalace.adapters;
+
+import com.carol.pastriespalace.models.HomeVerModel;
+
+import java.util.ArrayList;
+
+public interface UpdateVerticalRec {
+    public void callBack(int position, ArrayList<HomeVerModel> list);
+}
